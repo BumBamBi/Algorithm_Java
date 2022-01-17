1fecf1f9719dfe0e41d84f860a064a5b4a8943a0
BumBamBi <dlrhkddn0325@gmail.com>
2022-01-17 16:57:27 +0900

    solve 1330

---
+++ BaeckJoon/if문/_1330.java
@@ -0,0 +1,20 @@
+package BaeckJoon.if문;
+
+import java.util.Scanner;
+
+public class _1330 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        if(a > b){
+            System.out.println(">");
+        }else if(a < b){
+            System.out.println("<");
+        }else{
+            System.out.println("==");
+        }
+    }
+}
--- BaeckJoon/한글테스트/Main.java
+++
@@ -1,7 +0,0 @@
-package BaeckJoon.한글테스트;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("zzz");
-    }
-}
