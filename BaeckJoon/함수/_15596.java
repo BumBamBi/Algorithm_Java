6519078f861427394f302d82e156ad6d15ebb05d
BumBamBi <dlrhkddn0325@gmail.com>
2022-01-23 23:48:55 +0900

    solve 15596

---
+++ BaeckJoon/함수/_15596.java
@@ -0,0 +1,12 @@
+package BaeckJoon.함수;
+
+public class _15596 {
+
+    public long sum(int[] a){
+        long sum =0;
+        for(int i:a){
+            sum += i;
+        }
+        return sum;
+    }
+}
